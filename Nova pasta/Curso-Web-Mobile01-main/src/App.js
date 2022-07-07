2cb206d8cc0a6d06acdd7e4b5c45fad5c980a0bc
unknown <Administrator@IBCLAB01.LOCAL>
2022-07-07 17:07:53 -0300

    teste

---
+++ Nova pasta/Curso-Web-Mobile01-main/src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+import Contato from './components/pages/Contato'
+import Empresa from './components/pages/Empresa'
+import Home from './components/pages/Home'
+import Section from './components/pages/Section'
+
+function App() {
+  return (
+    <Router>
+     <Navbar/>
+      <Switch>
+        
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/empresa">
+          <Empresa />
+        </Route>
+        <Route path="/contato">
+          <Contato />
+        </Route>
+      </Switch>
+    
+    </Router>
+  )
+}
+
+export default App
